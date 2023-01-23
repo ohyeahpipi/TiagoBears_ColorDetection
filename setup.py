950cd2eca007e1a8d63734bd24298f4109a76e0b
Alonso94 <ay20-5-1994@hotmail.com>
2023-01-23 17:14:03 +0000

    updates

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 d = generate_distutils_setup(
-    packages=['TiagoBears_grasp'],
+    packages=['TiagoBears_ColorDetection'],
     package_dir={'': 'src'}
 )
 setup(**d)
