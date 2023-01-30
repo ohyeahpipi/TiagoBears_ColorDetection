fde83bb725a6b1772d6f6c2ab00052fdafddc5ae
Alonso94 <ay20-5-1994@hotmail.com>
2023-01-30 16:59:00 +0000

    added topics

--- scripts/color_detection_node.py
+++ scripts/color_detection_node.py
@@ -1,1 +1,6 @@
-#!/usr/bin/env python
+#!/usr/bin/env python   
+import rospy
+from TiagoBears_ColorDetection.color_detector import ColorDetector
+rospy.init_node('ColorDetector', anonymous=True)
+color_detector = ColorDetector()
+color_detector.update_colors()
--- src/TiagoBears_ColorDetection/color_detector.py
+++ src/TiagoBears_ColorDetection/color_detector.py
@@ -1,33 +1,35 @@
 #!/usr/bin/env python
 import rospy
+import roslib
 
 from std_msgs.msg import String # for color
 from nav_msgs.msg import Odometry # for pose
+from sensor_msgs.msg import Image
+
+import cv2
+from cv_bridge import CvBridge, CvBridgeError
 
 class ColorDetector:
 	"""
 	This class detect the per-cube color and count them
 
 	"""
-	def __init__(self, id):
-		self.id=id
-		self._stacked = False
-  
-		self.pose = None
-		self._pose_topic = '/cube_{0}_odom'.format(id)
-		# continuously update the pose:
-		# subscribe to the pose estimation topic
-		self.pose_sub=rospy.Subscriber(self._pose_topic, Odometry, self.update_pose)
+	def __init__(self):
+		self.image_topic = '/xtion/rgb/image_raw'.format(id)
+		self.bridge = CvBridge()
 		
-		self._color_topic='/cube_{0}_color'.format(id)
-		# publish the color of each cube
-		self.color_pub=rospy.Publisher(self._color_topic, String, 1)
-
-	def update_pose(self, msg: Odometry):
-		""" A callback function to update the pose whenever the pose subscriber recieves a topic
-  		"""
-		self.pose = msg.pose.pose
+		self.color_pubs = []
+		for i in range(28):
+			self.color_pubs.append(rospy.Publisher('/cube_{0}_color'.format(i), String, 1))
   
-	def detect_color(self):
+	def update_colors(self):
 		""" A function to detect the color for the cube depedning on its position
   		"""
+		img=rospy.wait_for_message(self.image_topic, Image)
+		img=self.bridge.imgmsg_to_cv2(img, "bgr8")
+		cube_color=[]
+		## you can write you code here to detect the color per-cube here
+
+		# publish the color
+		for i in range(28):
+			self.color_pubs[i].publish(cube_color[i])
