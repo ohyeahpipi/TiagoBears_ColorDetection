574a55ad80e0d3eedb412fac1dd6d354f3ce0d6f
Sophie Lueth <sophie.lueth@stud.tu-darmstadt.de>
2023-03-28 11:19:20 +0000

    color detect in diff

--- scripts/color_detection_node.py
+++ scripts/color_detection_node.py
@@ -3,8 +3,8 @@ import rospy
 from TiagoBears_ColorDetection.color_detector import ColorDetectorServer
 rospy.init_node('ColorDetector', anonymous=True)
 color_detector = ColorDetectorServer()
-# color_detector.init(False)
+color_detector.init(False)
 # input("cube")
 # color_detector.check_left(False)
 rospy.spin()
-# color_detector.detect_cubes()
+color_detector.detect_cubes()
